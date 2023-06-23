dced7c76c8e96a5f7efdc2b38cdca1111895673f
masha-a-m <escritoramaree@gmail.com>
2023-06-23 14:10:54 -0700

    u

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,14 @@
  # file in TMP new task
 
 file { '/tmp/school':
-  content =>I love Puppet,
-  mode    => 0744,
-  owner   => www-data,
-  group   => www-data,
+
+  content => 'I love Puppet',
+ 
+  mode    => '0744',
+  
+  owner   => 'www-data',
+  
+  group   => 'www-data',
 }
 
 # fiest 
